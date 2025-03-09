0cffd344a9a7e0fc08ad1056d7935bcb860df800
maksimhrybok <maxgribok1204@gmail.com>
2025-03-09 09:07:10 +0200

    9.03 morning. Createrd a new project

---
+++ Models/TodoModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace todoApp.Models
+{
+    internal class TodoModel
+    {
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+		private bool _isDone;
+
+		public bool isDone
+		{
+			get { return _isDone; }
+			set { _isDone = value; }
+		}
+
+		private string _text;
+
+		public string Text
+		{
+			get { return _text; }
+			set { _text = value; }
+		}
+
+
+	}
+}
